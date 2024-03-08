9932cef4127f60d543d7eef29f0eca7ebe4742e2
crush em T <hentaiktvn321@gmail.com>
2024-03-08 23:21:45 +0700

    open all again

--- src/main/java/sgu/hrm/module_utilities/services/IUtilitiesService.java
+++ src/main/java/sgu/hrm/module_utilities/services/IUtilitiesService.java
@@ -2,12 +2,15 @@ package sgu.hrm.module_utilities.services;
 
 import sgu.hrm.module_utilities.PhanTrang;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public interface IUtilitiesService<T, R> {
-    List<T> xemDS(PhanTrang<T> phanTrang);
-
+    List<T> xemDS();
+    default List<T> xemDS(PhanTrang<T> phanTrang){
+        return new ArrayList<>();
+    };
     Optional<T> xemTheoId(int id);
 
     T them(R name);
